535170969c858c3f3d33d5b001011b9c0b0c06eb
Sharath Bennur <sharath.bennur@gmail.com>
2025-12-22 15:39:56 -0500

    Home page setup to at least MVP level

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css'
+import './Home.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         {/* Placeholder for the dashboard route */}
         <Route path="/dashboard" element={<div>Dashboard Coming Soon!</div>} />
       </Routes>
